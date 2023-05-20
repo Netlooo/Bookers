e9ece118eea996e39b4d496af56c34d7c91f4a74
Netlooo <130125070+Netlooo@users.noreply.github.com>
2023-05-20 13:03:25 +0000

    [add]　上の部分

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,12 +11,20 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
+    
   end
 
   def edit
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
   end
 
   private
